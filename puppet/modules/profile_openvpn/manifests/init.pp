0b2e54d4c047a37bf82a85e558b1a820d5a3d276
bjanssens <bjanssens@inuits.eu>
2016-09-13 14:33:36 +0200

    Roles: VPS: Added profile_openvpn
    Signed-off-by: bjanssens <bjanssens@inuits.eu>

---
+++ puppet/modules/profile_openvpn/manifests/init.pp
@@ -0,0 +1,9 @@
+#
+# Profile_openvpn
+#
+# A profile to setup client or server config for openvpn
+# Also manages the firewall rules and the sysconfig
+#
+class profile_openvpn () {
+
+} # class profile_openvpn
--- puppet/modules/roles/manifests/vps.pp
+++ puppet/modules/roles/manifests/vps.pp
@@ -1,11 +1,13 @@
 #
 # VPS role
 # Node that does apache proxy to the internal bbqnetwork
+# Also contains a openvpn client
 #
 class roles::vps () {
 
   include ::profile_base
   include ::profile_apache
+  include ::profile_openvpn
 
 } # class roles::vps
 
